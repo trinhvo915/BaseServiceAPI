e7dd3e9c5fc8f148070fb27682593090415589d2
vovantrinhdhbkdn@gmail.com <Trinh Vo>
2022-11-20 16:41:52 +0700

    add send email when sign up user

---
+++ src/main/java/trinh/vo/van/constant/EmailConstants.java
@@ -0,0 +1,12 @@
+package trinh.vo.van.constant;
+
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class EmailConstants {
+    public static final String SIGN_UP_USER_TEMPLATE_MAIL = "templates/email/sign-up-user.html";
+    public static final String EMAIL = "email";
+    public static final String FULLNAME = "fullName";
+}
--- src/main/java/trinh/vo/van/service/user/UserServiceImpl.java
+++ src/main/java/trinh/vo/van/service/user/UserServiceImpl.java
@@ -3,12 +3,17 @@ package trinh.vo.van.service.user;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import trinh.vo.van.constant.EmailConstants;
 import trinh.vo.van.model.dto.response.user.UserResponses;
 import trinh.vo.van.model.entity.user.User;
 import trinh.vo.van.model.filter.UserFilter;
 import trinh.vo.van.repository.UserRepository;
+import trinh.vo.van.service.email.IEmailService;
+import trinh.vo.van.utils.ThreadUtil;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Objects;
 
 @Service
@@ -16,6 +21,7 @@ import java.util.Objects;
 public class UserServiceImpl implements UserService{
 
     private final UserRepository userRepository;
+    private final IEmailService emailService;
 
     @Override
     public Boolean existsByUsername(String username) {
@@ -42,7 +48,23 @@ public class UserServiceImpl implements UserService{
     @Override
     @Transactional
     public User save(User user) {
+        ThreadUtil.runAsync(() -> sendEmailSignUpUser(user));
+
         return userRepository.save(user);
+    }
+
+    private void sendEmailSignUpUser(User user){
+        String subject = "Sign Up User";
+        Map<String, Object> emailContent = new HashMap<>();
+
+        emailContent.put(EmailConstants.EMAIL, user.getEmail());
+        emailContent.put(EmailConstants.FULLNAME, user.getFullName());
+
+        emailService.sendEmail(new String[]{user.getEmail()}
+                , new String[0]
+                , subject
+                , EmailConstants.SIGN_UP_USER_TEMPLATE_MAIL
+                , emailContent);
     }
 
     @Override
